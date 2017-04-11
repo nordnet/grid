121d7498e331e26e0e973c5a13eccebc44d403e2
Vladimir Starkov <iamstarkov@gmail.com>
2017-04-11 12:40:14 +0200

    fix export entrypoint

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+export { default as Row } from './row';
+export { default as Col } from './col';
+export { default as Grid } from './grid';
+
 import Row from './row';
 import Col from './col';
 import Grid from './grid';
 
-export const grid = {
+export default {
   Row,
   Col,
   Grid,
 };
-
-export default grid;
